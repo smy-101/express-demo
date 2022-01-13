ed500f121cb26604eb260be15fd73e011c2e8b5c
smy-101 <shimy1011@foxmail.com>
2022-01-13 14:37:57 +0800

    尝试使用typescript改写

--- app2.ts
+++ app2.ts
@@ -1,13 +1,46 @@
-// const express = require('express')
-import express from 'express';
+import {ErrorRequestHandler, RequestHandler} from 'express-serve-static-core';
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!');
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+console.log('app2.ts');
+console.log('11111');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+} as RequestHandler);
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+} as ErrorRequestHandler);
+
+module.exports = app;
